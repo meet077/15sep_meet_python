b234d1588be15a46c60e19c769bd65c7d1e6d59d
meet077 <113657538+meet077@users.noreply.github.com>
2023-02-06 21:16:16 +0530

    Add files via upload

---
+++ assignment/module-3.1/3.c
@@ -0,0 +1,50 @@
+//WAP to find area of circle, rectangle and triangle
+
+/*#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float radius,area;
+    printf("Enter the radius of the circle\n");
+    scanf("%f",&radius);
+
+    area=3.14 * radius * radius;
+
+    printf("Area of the circle is %f\n",area);
+}*/
+
+
+
+
+/*#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float l,w,area;
+    printf("Enter the Length of the rectangle\n");
+    scanf("%f",&l);
+    printf("Enter the Width of the rectangle\n");
+    scanf("%f",&w);
+
+    area=l*w;
+
+    printf("Area of the circle is %f\n",area);
+}*/
+
+
+
+
+/*#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float b,h,area;
+    printf("Enter the Base of the triangle\n");
+    scanf("%f",&b);
+    printf("Enter the heigth of the triangle\n");
+    scanf("%f",&h);
+
+    area=b*h/2;
+
+    printf("Area of the circbe is %f\n",area);
+}*/
---
+++ assignment/module-3.1/4.c
@@ -0,0 +1,10 @@
+// WAP to find simple interest 
+
+#include <stdio.h>
+void main()
+{
+    float SI;
+	float P = 1, R = 1, T = 1;
+	SI = (P * T * R) / 100;
+	printf("Simple Interest = %f\n", SI);
+}
