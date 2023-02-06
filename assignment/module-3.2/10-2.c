25522fc905b6002f10049f376881027e48879c04
meet077 <113657538+meet077@users.noreply.github.com>
2023-02-06 21:22:46 +0530

    Add files via upload

---
+++ assignment/module-3.2/10-2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void main()
+{
+    char ch;
+    printf("Enter any alphabet: ");
+    scanf("%c", &ch);
+
+    switch(ch)
+    {
+        case 'a': 
+            printf("Vowel");
+            break;
+        case 'e': 
+            printf("Vowel");
+            break;
+        case 'i': 
+            printf("Vowel");
+            break;
+        case 'o': 
+            printf("Vowel");
+            break;
+        case 'u': 
+            printf("Vowel");
+            break;
+        default: 
+            printf("Consonant");
+    }
+
+}
